f144bdf8d46d696c0fdf2684807d490e8512a289
Черепахин Василий <vasi.che@gmail.com>
2024-02-26 23:06:21 +0500

    add MulicastCopyCatalogRouteBuilder.java

---
+++ src/main/java/com/codeusingjava/springbootcamel/route/MulicastCopyCatalogRouteBuilder.java
@@ -0,0 +1,22 @@
+package com.codeusingjava.springbootcamel.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MulicastCopyCatalogRouteBuilder extends RouteBuilder {
+	private String baseDstDir = "file:/tmp/testarea/multicast";
+	@Override
+	public void configure() throws Exception {
+		from("file:/tmp/testarea/multicast/srcDir")
+				.multicast().to(baseDstDir+"/dstDir1", baseDstDir+"/dstDir2")
+				.log("headers: ${headers.CamelFileName}")
+				.log("exchange: ${exchange}")
+				.log("id: ${id}")
+				.log("route id: ${routeId}")
+				.log("exchange id: ${exchangeId}")
+				.log("body: ${body}")
+				.log("headers: ${headers}")
+		;
+	}
+}
