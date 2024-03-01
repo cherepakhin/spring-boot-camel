bf43a58bf12409995df23a2882acee50d1dea3f9
Черепахин Василий <vasi.che@gmail.com>
2024-03-01 13:26:36 +0500

    add baseDir in MulicastCopyCatalogRouteBuilder.java

--- src/main/java/ru/perm/v/springbootcamel/route/MulicastCopyCatalogRouteBuilder.java
+++ src/main/java/ru/perm/v/springbootcamel/route/MulicastCopyCatalogRouteBuilder.java
@@ -5,17 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MulicastCopyCatalogRouteBuilder extends RouteBuilder {
-	private String baseDstDir = "file:/tmp/testarea/multicast";
+	private String baseDir = "file:/tmp/testarea/multicast";
 	@Override
 	public void configure() throws Exception {
-		from("file:/tmp/testarea/multicast/srcDir")
-				.multicast().to(baseDstDir+"/dstDir1", baseDstDir+"/dstDir2")
+		from(baseDir+ "/srcDir")
+				.multicast().to(baseDir+"/dstDir1", baseDir+"/dstDir2")
 				.log("headers: ${headers.CamelFileName}")
 				.log("exchange: ${exchange}")
 				.log("id: ${id}")
 				.log("route id: ${routeId}")
 				.log("exchange id: ${exchangeId}")
-//				.log("body: ${body}")
+//				.log("body: ${body}") // for logging content of files
 				.log("headers: ${headers}")
 		;
 	}
